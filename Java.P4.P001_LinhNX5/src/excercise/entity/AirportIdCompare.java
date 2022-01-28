07b6ef435d1628f356e8df854dce8df63705e6b2
linh <thuichan123@gmail.com>
2022-01-28 17:07:48 +0700

    upload file

---
+++ Java.P4.P001_LinhNX5/src/excercise/entity/AirportIdCompare.java
@@ -0,0 +1,12 @@
+package excercise.entity;
+
+import java.util.Comparator;
+
+public class AirportIdCompare implements Comparator<Airports> {
+
+	@Override
+	public int compare(Airports o1, Airports o2) {
+		return o1.getId().compareToIgnoreCase(o2.getId());
+	}
+
+}
